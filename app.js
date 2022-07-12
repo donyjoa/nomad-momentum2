6a991bc5b49d3878ac6d67b773fe140f8fc1f0b9
pig <dudgh5184@naver.com>
2022-07-12 23:38:13 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const userName = loginInput.value;
+  loginForm.classList.add("hidden");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
