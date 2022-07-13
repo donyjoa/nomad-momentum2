d6dd40d65d8b29e70e59a974bf38c96b1bed2ad4
pig <dudgh5184@naver.com>
2022-07-13 23:24:43 +0900

    ~#4.5

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HiDDEN_CALSSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const userName = loginInput.value;
-  loginForm.classList.add("hidden");
+  localStorage.setItem("username", userName);
+  loginForm.classList.add(HiDDEN_CALSSNAME);
+  greeting.innerText = `Hello ${userName}`;
+  greeting.classList.remove(HiDDEN_CALSSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
