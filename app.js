a4b668a4def50d43f6c1b24434a890c3d0cd0343
pig <dudgh5184@naver.com>
2022-07-17 23:57:29 +0900

    ~4.7

--- app.js
+++ app.js
@@ -2,15 +2,29 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HiDDEN_CALSSNAME = "hidden";
+const HIDDEN_CALSSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  const userName = loginInput.value;
-  localStorage.setItem("username", userName);
-  loginForm.classList.add(HiDDEN_CALSSNAME);
-  greeting.innerText = `Hello ${userName}`;
-  greeting.classList.remove(HiDDEN_CALSSNAME);
+  loginForm.classList.add(HIDDEN_CALSSNAME);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  paintGreetings();
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CALSSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CALSSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings();
+}
